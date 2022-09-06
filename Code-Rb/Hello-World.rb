25c1c7c343ee745ee233692ba5b38ffdb856bc2a
arthur duarte <arthurddduarte86@gmail.com>
2022-09-05 21:51:46 -0300

    newex

---
+++ Code-Rb/Hello-World.rb
@@ -0,0 +1,8 @@
+=begin
+    Description:
+Make a simple function called greet that returns the most-famous "hello world!".
+=end
+# Write a function "greet" that returns "hello world!"
+def greet()
+    p "hello world!"
+end
