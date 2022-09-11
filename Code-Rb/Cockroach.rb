512dcf8c9e7f2f670e589dbb1c5c15a407de5830
arthur duarte <arthurddduarte86@gmail.com>
2022-09-11 19:05:10 -0300

    news

---
+++ Code-Rb/Cockroach.rb
@@ -0,0 +1,18 @@
+=begin
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+=end
+
+def cockroach_speed(s) return (s/0.036).floor
+  # Good Luck!
+end
+
+# ou solução abaixo
+
+def cockroach_speed(s)
+  (s / 0.036).to_i
+end
